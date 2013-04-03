a46887810b354a2a8dac64d89b8435ba98d05ab1
Daniel De Marco <ddm@bartol.udel.edu>
2013-04-03 09:36:57 -0400

    whitespace, cosmetic changes

--- lib/puppet/provider/htpasswd/parsed.rb
+++ lib/puppet/provider/htpasswd/parsed.rb
@@ -2,9 +2,9 @@ require 'puppet/provider/parsedfile'
 htpasswd_file = "/etc/httpd/conf/htpasswd"
 
 Puppet::Type.type(:htpasswd).provide(
-  :parsed, 
-  :parent => Puppet::Provider::ParsedFile, 
-  :default_target => htpasswd_file, 
+  :parsed,
+  :parent => Puppet::Provider::ParsedFile,
+  :default_target => htpasswd_file,
   :filetype => :flat
 ) do
 
@@ -13,6 +13,4 @@ Puppet::Type.type(:htpasswd).provide(
   text_line :comment, :match => /^#/;
   text_line :blank, :match => /^\s*$/;
   record_line :parsed, :fields => %w{name cryptpasswd}, :joiner => ':', :separator => ':'
-
 end
-
--- lib/puppet/type/htpasswd.rb
+++ lib/puppet/type/htpasswd.rb
@@ -1,34 +1,34 @@
 module Puppet
   newtype(:htpasswd) do
     @doc = "Manage an Apache style htpasswd file
-  
-    htpasswd { \"user\":
-      ensure => present,
-      cryptpasswd => \"MrC7Aq3qPKPaK\",
-      target => \"/etc/httpd/conf/htpasswd\",
+
+    htpasswd { 'user':
+      ensure      => present,
+      cryptpasswd => 'encrypted password',
+      target      => '/etc/httpd/conf/htpasswd',
     }"
 
     ensurable
 
     newparam(:name, :namevar => true) do
-      desc "The user to manage"
+      desc "The resource name"
       isnamevar
     end
 
     newproperty(:cryptpasswd) do
       desc "The encrypted password for the given user"
+      isrequired
     end
 
     newproperty(:target) do
       desc "Location of the htpasswd file"
-      defaultto {
+      defaultto do
         if @resource.class.defaultprovider.ancestors.include?(Puppet::Provider::ParsedFile)
           @resource.class.defaultprovider.default_target
         else
           nil
         end
-      }
+      end
     end
-
   end
 end
