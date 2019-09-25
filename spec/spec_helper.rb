3fe3c1760fdc9cd1495377db66343f664c5d9c31
Tomas Barton <barton.tomas@gmail.com>
2019-09-25 11:53:50 +0200

    fix tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require 'rubygems'
 require 'puppetlabs_spec_helper/module_spec_helper'
+
+RSpec.configure do |config|
+  config.hiera_config = 'spec/fixtures/hiera/hiera.yaml'
+  config.before :each do
+    Facter.clear
+    Facter.clear_messages
+  end
+end
